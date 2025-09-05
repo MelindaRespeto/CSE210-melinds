351b1f0ac7b15fdd042fa0e53ef1928a926bf5ac
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-09-05 13:30:03 +0800

    commit

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("What is your first name");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is your first name");
+        string first = Console.ReadLine();
     }
 }
